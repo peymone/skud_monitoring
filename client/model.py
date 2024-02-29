f299a48a83549e6278d6479de5263b53cffd9c1d
Vladimir Reymer <vladimireymer@gmail.com>
2024-02-29 12:00:47 +0700

    Class for getting data from checkers and return values
        * get_servicesStatuses method - Get data services statuses from Lyrix and ostel checkers

---
+++ client/model.py
@@ -0,0 +1,29 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from servicesChecker import services_checker
+
+
+class Model:
+    """Class for getting data from checkers and return values"""
+
+    def __init__(self) -> None:
+        pass
+
+    def get_servicesStatuses(self) -> tuple[dict[str, tuple[str]], dict[str, tuple[str]] | str]:
+        """Get data services statuses from Lyrix and ostel checkers"""
+
+        # Thread pool for parallel checkers execution
+        with ThreadPoolExecutor(2) as pool:
+            lyrixChecker_thread = pool.submit(
+                services_checker.get_lyrixServices_status)
+            ostelChecker_thread = pool.submit(
+                services_checker.get_ostelServices_status)
+
+            # Get results from threads
+            lyrixServices_status = lyrixChecker_thread.result()
+            ostelServices_status = ostelChecker_thread.result()
+
+        return lyrixServices_status, ostelServices_status
+
+
+model = Model()
