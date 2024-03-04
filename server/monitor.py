15a1dc51ec1d1545ad7e34005adf59bb29050eb8
Vladimir Reymer <vladimireymer@gmail.com>
2024-03-04 20:19:27 +0700

    Server backbone

--- server/monitor.py
+++ server/monitor.py
@@ -1,11 +1,131 @@
 from concurrent.futures import ThreadPoolExecutor
 from threading import Thread, Event
+from json import load
+import socket
 
 # Import application modules
 from servers_check.checker import servers_checker
 from api_check.checker import api_checker
 from pretifier import rich
 from logger import logger
+
+
+class Server:
+    def __init__(self, port) -> None:
+        self.__host = socket.gethostbyname(socket.gethostname())
+        self.__port = port
+        self.__server_status = False
+        self.__server_clients = dict()
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.__host, self.__port))
+
+    def start(self):
+        try:
+            self.server_socket.listen()
+            self.__server_status = True
+
+            print('\n')
+            rich.show(
+                f"Server is running on {self.__host}:{self.__port}. Waiting for connections")
+            logger.log.debug(
+                f"Server is running on {self.__host}:{self.__port}. Waiting for connections")
+            print('\n')
+
+            while self.__server_status is True:
+                client_socket, client_address = self.server_socket.accept()
+                client_thread = Thread(target=self.__recieve_messages_fromClient, args=(
+                    client_socket, client_address), daemon=True)
+                client_thread.start()
+
+        except OSError:
+            pass
+
+    def stop(self):
+        if self.__server_status is False:
+            pass
+        else:
+            pass
+
+    def __recieve_messages_fromClient(self, cleint_socket, client_address):
+        try:
+            client_host = cleint_socket.recv(1024).decode('utf-8')
+            self.__server_clients[client_host] = (
+                cleint_socket, client_address)
+            logger.log.debug(
+                f"{client_address} esablished connection with name {client_host}")
+
+            while client_host is self.__server_clients.keys():
+                message = cleint_socket.recv(1024).decode('utf-8')
+                match message:
+                    case 'services_statuses':
+                        services_statuses_json = cleint_socket.recv(
+                            1024).decode('utf-8')
+                        services_statuses = load(services_statuses_json)
+
+                        lyrixServices_statusMsg = [
+                            status[0] for status in services_statuses[0].values()]
+                        ostelServices_statusMsg = [
+                            status[0] for status in services_statuses[1].values()]
+
+                        if ('Down' in lyrixServices_statusMsg) or ('pid not found' in lyrixServices_statusMsg):
+                            rich.show(
+                                f"Lyrix services status on {client_host}:", lvl='warning')
+                            logger.log.warning(
+                                f"Lyrix services status on {client_host}:")
+                            print('\n')
+
+                            for service_name, status in services_statuses[0].items():
+                                if status[0] == 'Down':
+                                    rich.show(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}", lvl='error')
+                                    logger.log.error(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}")
+                                elif status[0] == 'pid not found':
+                                    rich.show(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}", lvl='warning')
+                                    logger.log.warning(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}")
+                        else:
+                            rich.show(
+                                f"{client_host.ljust(45)}All lyrix services is up", lvl='info')
+                            logger.log.info(
+                                f"{client_host.ljust(45)}All lyrix services is up")
+                            print('\n')
+
+                        if ('Down' in ostelServices_statusMsg) or ('Warning' in ostelServices_statusMsg):
+                            rich.show(
+                                f"Lyrix services status on {client_host}:", lvl='warning')
+                            logger.log.warning(
+                                f"Lyrix services status on {client_host}:")
+                            print('\n')
+
+                            for service_name, status in services_statuses[1].items():
+                                if status[0] == 'Down':
+                                    rich.show(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}", lvl='critical')
+                                    logger.log.critical(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}")
+                                elif status[0] == 'Warning':
+                                    rich.show(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}", lvl='warning')
+                                    logger.log.warning(
+                                        f"{service_name.ljust(45)}{status[0].ljust(40)}last log was {status[1]}")
+                        else:
+                            rich.show(
+                                f"{client_host.ljust(45)}All ostel services is up", lvl='info')
+                            logger.log.info(
+                                f"{client_host.ljust(45)}All ostel services is up")
+                            print('\n')
+
+        except ConnectionResetError:
+            logger.log.debug(
+                f"{client_address} with name {client_host} disconnected")
+
+            del self.__server_clients[client_host]
+
+        except ConnectionAbortedError:
+            pass
 
 
 class Monitor:
@@ -160,6 +280,10 @@ class Monitor:
             rich.show("Monitor is not working at the moment", lvl='warning')
 
         print('\n')
+
+    def start_server(self, port):
+        self.__server = Server(port)
+        self.__server.start()
 
     def server_status(self):
         pass
