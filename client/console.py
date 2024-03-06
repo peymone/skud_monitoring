e8bb750e2c6923aaf32092f11e6d4784c027357b
Vladimir Reymer <vladimireymer@gmail.com>
2024-03-06 22:50:25 +0700

    Unimportant changes for console

--- client/console.py
+++ client/console.py
@@ -1,8 +1,11 @@
 from sys import argv
+
 from client import model
 
 
 class Console:
+    """Class for user input and commands execution"""
+
     def __init__(self) -> None:
         self.__command = None
         self.__commands = {
@@ -14,33 +17,42 @@ class Console:
 
     def show_commands(self) -> None:
         """Show all available commands"""
+
         print('\n')
 
         for command, description in self.__commands.items():
-            print(f"{command.ljust(60)}{description}")
+            print(f"{command.ljust(45)}{description}")
 
         print('\n')
 
     def commands_handler(self) -> None:
+        """Enter and execute command"""
+
         while self.__command != 'exit':
-            try:
-                # Enter commands until 'exit'
+            try:  # Catch ctrl + c
+
                 self.__command = input("Enter command /> ")
 
-                # Execute command
-                match self.__command:
+                match self.__command:  # Execute commands
+
                     case 'commands': self.show_commands()
+
                     case 'stop': model.stop_client()
+
                     case 'status':
                         services_statuses = model.get_servicesStatuses()
                         print('\n')
+
                         for status in services_statuses:
-                            for service_name, result in status.items():
-                                print(service_name.ljust(50),
-                                      result[0].ljust(40), f'last log was {result[1]}')
+                            for service_name, status in status.items():
+                                print(service_name.ljust(45), status[0].ljust(
+                                    40), f'last log was {status[1]}')
+
                             print('\n')
 
-            except KeyboardInterrupt:
+                        print('\n')
+
+            except KeyboardInterrupt:  # Ctrl + C
                 self.__command = 'exit'
 
         print('\n')
@@ -49,7 +61,7 @@ class Console:
 if __name__ == '__main__':
     client_console = Console()
 
-    if len(argv) > 1:
+    if len(argv) > 1:  # Custom or standard server ip and port
         server_host, server_port = argv[1], int(argv[2])
     else:
         server_host, server_port = '10.40.21.3', 9186
